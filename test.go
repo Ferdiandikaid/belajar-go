889482e7d15736d144c1edb537c4753306847706
Andhika Ferdiansyah <ferdiandikaid@gmail.com>
2024-02-26 09:39:12 +0700

    Mulai belajar sesi 3 GO

--- test.go
+++ test.go
@@ -1,22 +1,31 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func isOne(nomor int) {
+// 	if nomor == 1 {
+// 		fmt.Print("Ini satu")
+// 	} else {
+// 		fmt.Print("Ini bukan satu")
+// 	}
+// }
+
+// func main() {
+// 	var (
+// 		nomor1 int
+// 		nomor2 int
+// 	)
+// 	fmt.Println("Masukkan nomor 1:")
+// 	fmt.Scan(&nomor1)
+// 	isOne(nomor1)
+// }
+
 package main
 
-import (
-	"fmt"
-)
-
-func isOne(nomor int) {
-	if nomor == 1 {
-		fmt.Print("Ini satu")
-	} else {
-		fmt.Print("Ini bukan satu")
-	}
-}
+import "fmt"
 
 func main() {
-	var (
-		nomor1 int
-	)
-	fmt.Println("Masukkan nomor 1:")
-	fmt.Scan(&nomor1)
-	isOne(nomor1)
+	fmt.Println("Hello World")
 }
